105eabe23820f8ce7ea40ff88f20f5f77b148cea
alexanderpascual110 <24308060610056@cetis61.edu.mx>
2025-10-15 08:56:45 -0500

    cambioos

--- appy.py
+++ appy.py
@@ -12,6 +12,9 @@ def base():
 def formulario():
     return render_template("formulario.html")
 
+@app.route("/roblox")
+def roblox():
+    return render_template("roblox.html")
 
 if __name__=="__main__":
     app.run(debug=True)
